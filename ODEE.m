7b33738aeb27fc45c479d0f95f03dc4e5315a0a3
Nikola Hardi <atlantic777@lugons.org>
2013-11-13 00:22:18 +0100

    Promenjen naziv parametra iz x u t

--- ODEE.m
+++ ODEE.m
@@ -1,4 +1,4 @@
-function [t,y] = ODEE(F,t_limits,y0,h,h_limits,abserr,relerr)
+function [x,y] = ODEE(F,x_limits,y0,h,h_limits,abserr,relerr)
 
 c1=1/4;
 c2=3/8;
@@ -8,32 +8,32 @@ a3=2/3;
 
 %F=@(t,x)3.*exp(-t)-0.4*x;
 i=1;
-t(1) = t_limits(1);
+x(1) = x_limits(1);
 y(1,:) = y0;
 
-while t(i) < t_limits(2)
+while x(i) < x_limits(2)
     
-    k1=F(t(i),y(i,:))*h;
-    k2=F(t(i)+a2*h,y(i,:)+a2*k1)*h;
-    k3=F(t(i)+a3*h,y(i,:)+a3*k2)*h;
+    k1=F(x(i),y(i,:))*h;
+    k2=F(x(i)+a2*h,y(i,:)+a2*k1)*h;
+    k3=F(x(i)+a3*h,y(i,:)+a3*k2)*h;
     
     e=k3;
     fleg=1;
     
     y(i+1,:) = y(i,:)+c1*k1+c2*k2+c3*k3;
-    t(i+1) = t(i) + h;
+    x(i+1) = x(i) + h;
     
     if abs(e) > (abserr+relerr*abs(y(i+1,:)))
         h = h * 0.5;
         if h < h_limits(1)
-            h = h_limits(1)
+            h = h_limits(1);
         end
         fleg=0;
     end
     if abs(e)<((1/4)*(abserr+relerr*abs(y(i+1,:))))
             h = h*2;
             if h > h_limits(2)
-                h = h_limits(2)
+                h = h_limits(2);
             end
            %fleg = 0;
      end
